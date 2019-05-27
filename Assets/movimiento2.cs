cd762eec8a504d1ec7d694c736cf9dae9138e1ab
jonathancastillo99 <50933841+jonathancastillo99@users.noreply.github.com>
2019-05-27 18:19:10 -0500

    Initial commit

---
+++ Assets/movimiento2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento2 : MonoBehaviour
+{
+    #region Variables
+    float velocidad = 3f;
+    public float tiempo = 0f;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempo += 0.5f * Time.deltaTime;
+
+        if (tiempo >= 3.5f)
+
+        {
+            transform.Translate(Vector3.forward * velocidad);
+        }
+
+
+    }
+} //fin de la clase movimiento
