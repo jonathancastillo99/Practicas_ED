a18b7c646d5d138bb8b4948fb69cda07095df4a8
jonathancastillo99 <50933841+jonathancastillo99@users.noreply.github.com>
2019-06-06 12:10:50 -0500

    Practica 2
    Cubo desplazamiento

---
+++ Assets/Práctica 2/Script/MovX.cs
@@ -0,0 +1,77 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovX : MonoBehaviour
+{   float velocidad = 0.3f;
+    int tope = 4;
+    bool direccion, direccion2;
+
+   private void Start()
+{
+    direccion = true;
+
+}
+// Update is called once per frame
+void Update()
+    {
+
+    if (direccion == true)
+    {
+        if (transform.position.x < tope)
+        {
+            transform.Translate(Vector3.right * velocidad);
+        }
+
+        if (transform.position.x >= tope)
+        {
+            direccion = false;
+        }
+
+    }
+
+        if (direccion == false)
+        {
+            if (transform.position.y < 6)
+            {
+                transform.Translate(Vector3.up * velocidad);
+            }
+            if (transform.position.y >= 6)
+            {
+                direccion2 = true;
+            }
+        }
+
+
+        if (direccion2 == true)
+        {
+            if (transform.position.x >= 0)
+            {
+                transform.Translate(Vector3.left * velocidad);
+            }
+            if (transform.position.x <= 0)
+            {
+                direccion2 = false;
+            }
+        }
+
+
+
+
+
+
+        if (direccion == false)
+        {
+            if (transform.position.x <= 0)
+            {
+                transform.Translate(Vector3.down * velocidad);
+            }
+            if (transform.position.y <= 0)
+            {
+                direccion = true;
+            }
+        }
+
+    }
+}
+
