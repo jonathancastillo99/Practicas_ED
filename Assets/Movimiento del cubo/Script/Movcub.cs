2add36d5cfb7649c0164c25c6e6aff1cec953d99
jonathancastillo99 <50933841+jonathancastillo99@users.noreply.github.com>
2019-06-13 17:35:56 -0500

    Arrays de movimiento
    Aplicación de los arrays para el movimiento de un objeto basado en puntos

--- Assets/Movimiento del cubo/Script/Movcub.cs
+++ Assets/Movimiento del cubo/Script/Movcub.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Movcub : MonoBehaviour
 {
     public float velocidad;
-    private float xAxis, zAxis;
+   
     // Start is called before the first frame update
     void Start()
     {
