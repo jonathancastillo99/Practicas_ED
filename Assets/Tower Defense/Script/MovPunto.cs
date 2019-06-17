6dc3078e2b99aca02bc34c3e10f7c0f1db6ace89
jonathancastillo99 <50933841+jonathancastillo99@users.noreply.github.com>
2019-06-16 20:52:58 -0500

    Movimiento escenario
    Se ha añadido un escenario con un objeto en movimiento por medio de puntos, al estilo de un guardián.

---
+++ Assets/Tower Defense/Script/MovPunto.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovPunto : MonoBehaviour
+{
+    public float velocidad;
+    public Transform sierra;
+    public Transform[] puntos;
+    public Transform target;
+    public int indiceTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Se convierte en el elemento inicial el indice 1
+        target = puntos[1];
+        //.position lee la posición
+        sierra.position = target.position;
+        velocidad = velocidad * Time.deltaTime;
+        indiceTarget = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - sierra.position;
+        //distance va a medir la distancia emntre el objeto a mover y el objetivo 
+        float distance = Vector3.Distance(sierra.position, target.position);
+        //Se va a trasladar tomando en cuenta la normalizacion de la velocidad midiendo su espacio en el mundo
+        sierra.Translate(dir.normalized * velocidad, Space.World);
+
+        //si la distancia es menor o igual a 0.5 se le suma uno a indice, que es el que indica hacia que punto se va a mover
+        if (distance <= 0.5f)
+        {
+            //.Length es para que el sistema lea la extensión total del array lee cada nunero o valor dentro del array desde 0 hasta 4
+            //El -1 se obliga a que se lea solo hasta el último punto, no se sobrecarga el array
+            if (indiceTarget >= puntos.Length - 1)
+            {
+                indiceTarget = 0;
+                target = puntos[indiceTarget];
+            }
+
+            indiceTarget++;
+            target = puntos[indiceTarget];
+        }
+    }
+}
